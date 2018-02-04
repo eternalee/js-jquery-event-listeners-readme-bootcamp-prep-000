5e6629484bb82067f332ac13bf1243f990085a19
Eterna <one.eternity@gmail.com>
2018-02-04 22:37:54 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -11,8 +11,26 @@ function frameIt() {
   })
 }
 
+function pressIt() {
+  $('#typing').on('keydown', function(key) {
+    if (key.which === 71) {
+      alert("G Alert!");
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.")
+  })
+}
+
 $(document).ready(function(){
 
 // call functions here
+getIt();
+frameIt();
+pressIt();
+submitIt();
 
 });
